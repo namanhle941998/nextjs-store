7d3690f1ff954c1e2bb5104bbfa48ce9f44b01c4
anhln <namanhle941998@gmail.com>
2024-12-17 00:29:07 +0700

    Setup components

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,13 @@
-function HomePage() {
-  return <div>HomePage</div>;
-}
+import { Button } from "@/components/ui/button";
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1 className="text-3xl">HomePage</h1>
+      <Button variant="outline" size="lg" className="capitalize m-8">
+        Click me
+      </Button>
+    </div>
+  );
+};
 export default HomePage;
---
+++ components/navbar/Navbar.tsx
@@ -0,0 +1,22 @@
+import Container from "../global/Container";
+import CartButton from "./CartButton";
+import DarkMode from "./DarkMode";
+import LinksDropdown from "./LinksDropdown";
+import Logo from "./Logo";
+import NavSearch from "./NavSearch";
+
+const Navbar = () => {
+  return (
+    <nav className="border-b">
+      <Container className="flex flex-col sm:flex-row sm:justify-between sm:items-center flex-wrap py-8">
+        <Logo />
+        <NavSearch />
+        <div className="flex gap-4 items-center"></div>
+        <CartButton />
+        <DarkMode />
+        <LinksDropdown />
+      </Container>
+    </nav>
+  );
+};
+export default Navbar;
