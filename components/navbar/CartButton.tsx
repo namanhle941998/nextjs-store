1ee29e9f59b6c7eca0fd15ffc8ad9256e5628922
anhln <namanhle941998@gmail.com>
2024-12-24 08:31:50 +0700

    Setup

--- components/navbar/CartButton.tsx
+++ components/navbar/CartButton.tsx
@@ -1,4 +1,23 @@
-const CartButton = () => {
-  return <div>CartButton</div>;
+import Link from "next/link";
+import { Button } from "../ui/button";
+import { LuShoppingCart } from "react-icons/lu";
+
+const CartButton = async () => {
+  const numItemsInCard = 9;
+
+  return (
+    <Button
+      asChild
+      variant="outline"
+      size="icon"
+      className="flex justify-center items-center relative"
+    >
+      <Link href="/cart">
+        <span className="absolute -top-3 -right-3 bg-primary text-white rounded-full h-6 w-6 flex items-center justify-center text-xs">
+          {numItemsInCard}
+        </span>
+      </Link>
+    </Button>
+  );
 };
 export default CartButton;
